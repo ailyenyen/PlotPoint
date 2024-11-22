b635b74718f735a9a56ad357b2ae070382bc8b33
Jan Mayen Mallen <23-06458@g.batstate-u.edu.ph>
2024-11-22 20:35:19 +0800

    done

--- src/managers/BookManager.java
+++ src/managers/BookManager.java
@@ -9,6 +9,7 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
+
 public class BookManager {
 
     private final DatabaseHelper dbHelper = new DatabaseHelper();
